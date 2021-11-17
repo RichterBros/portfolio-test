b59153009b17f5a704ee6b0ed25d26585e6bad74
Erich Richter <erich.richter@gmail.com>
2021-11-16 16:28:47 -0800

    add cors

--- cors/index.js
+++ cors/index.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 const corsOptions = {
   // origin: "https://eloquent-boyd-bd99b4.netlify.app",
-  origin: "http://richterbrosmedia.com",
+  origin: "http://richterbrosmedia.com/video",
   optionsSuccessStatus: 200,
 };
 
